fac7cd8dc5c4b79b5cbe44e35d4e4afb31ea1aa6
kolyaka32 <nik.kazankov.05@mail.ru>
2025-04-03 21:45:56 +0300

    Updating scale system Reversing scale value Adding limitation to scale Drawing grid

--- src/computing/grid.cpp
+++ src/computing/grid.cpp
@@ -4,7 +4,7 @@
  */
 
 #include "grid.hpp"
-
+#include "../define.hpp"
 
 Grid::Grid(const Window& _target) {
     capture = false;
@@ -19,14 +19,23 @@ Grid::~Grid() {
 
 void Grid::draw(const Window& _target) const {
     _target.setDrawColor(WHITE);
-    _target.drawRect({localX(20), localY(20), 50*scale, 50*scale});
+    _target.drawRect({localX(20), localY(20), 50/scale, 50/scale});
+
+    // Rendering grid
+    for (int i=0; i < 20; ++i) {
+        _target.drawLine(localX(i*100), 0, localX(i*100), WINDOW_HEIGHT);
+    }
+
+    for (int i=0; i < 20; ++i) {
+        _target.drawLine(0, localY(i*100), WINDOW_WIDTH, localY(i*100));
+    }
 }
 
 void Grid::update(float mouseX, float mouseY) {
     if (capture) {
         // Keeping captured point at it place
-        centerX = captureX - mouseX/scale;
-        centerY = captureY - mouseY/scale;
+        centerX = captureX - mouseX*scale;
+        centerY = captureY - mouseY*scale;
         //SDL_Log("Press at %f : %f, get at %f : %f", mouseX, mouseY, centerX, centerY);
     }
 }
@@ -43,34 +52,31 @@ void Grid::unClick(float mouseX, float mouseY) {
 }
 
 void Grid::zoom(float mouseX, float mouseY, float wheelY) {
-    
-    centerX += mouseX/scale;
-    centerY += mouseY/scale;
+    centerX += mouseX*scale;
+    centerY += mouseY*scale;
 
-    //scale += wheelY;
-    if (scale > 0) {
-        scale *= SDL_powf(1.5, wheelY);
-    } else {
-        scale /= SDL_powf(1.5, -wheelY);
-    }
+    scale *= SDL_powf(1.5, -wheelY);
 
-    centerX -= mouseX/scale;
-    centerY -= mouseY/scale;
+    SET_MIN(scale, 0.1);
+    SET_MAX(scale, 20);
+
+    centerX -= mouseX*scale;
+    centerY -= mouseY*scale;
 }
 
 // Local-absolute transformations
 absolute Grid::absoluteX(local _x) const {
-    return _x / scale + centerX;
+    return _x * scale + centerX;
 }
 
 absolute Grid::absoluteY(local _y) const {
-    return _y / scale + centerY;
+    return _y * scale + centerY;
 }
 
 local Grid::localX(absolute _x) const {
-    return (_x - centerX) * scale;
+    return (_x - centerX) / scale;
 }
 
 local Grid::localY(absolute _y) const {
-    return (_y - centerY) * scale;
+    return (_y - centerY) / scale;
 }
